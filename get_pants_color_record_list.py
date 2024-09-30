5a3b8208302c47a12b07220f81cac6e4ab1c6aa7
oksy <qn8524kingxyf@gmail.com>
2024-09-30 16:19:07 +0800

    异步绘制

--- get_pants_color_record_list.py
+++ get_pants_color_record_list.py
@@ -1,8 +1,11 @@
 from typing import Tuple, Any
 from nonebot import on_regex
 from nonebot.rule import to_me
+from protocol_adapter.adapter_type import AdapterMessageEvent
 from utils.permission import white_list_handle
 from protocol_adapter.protocol_adapter import ProtocolAdapter
+from utils.push_manager import PushManager
+from utils.task_deliver import TaskDeliverManager
 from .database.pants_color import DBPantsColorInfo
 from .painter.pants_record_painter import PantsRecordPainter
 from nonebot.params import RegexGroup
@@ -19,15 +22,28 @@ get_pants_color_record_list.handle()(white_list_handle("pants_color"))
 get_pants_color_record_list.handle()(only_me)
 
 
+def generate_pic_task(**kwargs):
+    name = kwargs["name"]
+    bg_pic = kwargs["bg_pic"]
+    pants_color_list = DBPantsColorInfo.get_pants_color_list(name)
+    push_data = PushManager.PushData(
+        msg_type=kwargs["msg_type"],
+        msg_type_id=kwargs["msg_type_id"],
+        message=ProtocolAdapter.MS.image(PantsRecordPainter.generate_pants_record_pic(name, bg_pic, pants_color_list))
+    )
+    PushManager.notify(push_data)
+
+
 @get_pants_color_record_list.handle()
-async def _(params: Tuple[Any, ...] = RegexGroup()):
+async def _(event: AdapterMessageEvent, params: Tuple[Any, ...] = RegexGroup()):
     """获取咪莉娅胖次颜色记录"""
     name = params[0]
     if name not in names or names[name]["name"] is None:
         await get_pants_color_record_list.finish("无效目标名！")
-    pants_color_list = DBPantsColorInfo.get_pants_color_list(names[name]["name"])
-    message = ProtocolAdapter.MS.image(PantsRecordPainter.generate_pants_record_pic(
-        names[name]["name"],
-        names[name]["bg_pic"],
-        pants_color_list))
-    await get_pants_color_record_list.finish(message)
+    TaskDeliverManager.add_task(
+        generate_pic_task,
+        name=names[name]["name"],
+        bg_pic=names[name]["bg_pic"],
+        msg_type=ProtocolAdapter.get_msg_type(event),
+        msg_type_id=ProtocolAdapter.get_msg_type_id(event))
+    await get_pants_color_record_list.finish("正在绘制中。。。")
